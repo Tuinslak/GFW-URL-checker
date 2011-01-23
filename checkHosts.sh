80e8c6947cb2772b72d19e8392ea13d7c74e22c6
gfw@vm1.rootspirit.com <hi@tuinslak.be>
2011-01-23 15:59:09 +0100

    NL check @ $DATE

--- checkHosts.sh
+++ checkHosts.sh
@@ -4,16 +4,19 @@
 #
 # simple bash (c)
 # Thanks to Pieter Colpaert for the help
-# 
+#
 # Checks on 200 (found), and writes files
 
 #
-# Usage: 
+# Usage:
 # checkHosts <input file> <output file>
 #
 
+# clean up
+[ -e $2_nok.csv ] && rm $2_nok.csv
+[ -e $2_ok.csv ]  && rm $2_ok.csv
+
+# test
 while read a; do {
-
 	[[ `wget $a --user-agent="GFW checker (http://yeri.be/hr)" -O /dev/null 2>&1 | grep "200 OK"` == "" ]] && echo $a,nok >> $2_nok.csv || echo $a,ok >> $2_ok.csv
-
 } done < $1
