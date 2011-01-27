08ae55695606e541af3eab8f285321535c30f962
Yeri Tiete <yeri@tiete.be>
2011-01-28 00:26:10 +0100

    error handling & added country in UA

--- checkHosts.sh
+++ checkHosts.sh
@@ -8,10 +8,15 @@
 # Checks on 200 (found), and writes results to files
 # Github page: https://github.com/Tuinslak/GFW-URL-checker
 
+: ${1?"Usage: $0 <input file> <output file>"}
+
 #
 # Usage:
 # checkHosts <input file> <output file>
 #
+
+# get country variable
+source country.sh
 
 # clean up
 [ -e $2_nok.csv ] && rm $2_nok.csv
@@ -19,5 +24,5 @@
 
 # test
 while read a; do {
-	[[ `wget $a --user-agent="GFW-URL-checker (http://yeri.be/hr)" --no-check-certificate -O /dev/null 2>&1 | grep "200 OK"` == "" ]] && echo $a,`date -u +%d-%m-%Y,%H:%M:%S`,nok >> $2_nok.csv || echo $a,`date -u +%d-%m-%Y,%H:%M:%S`,ok >> $2_ok.csv
+	[[ `wget $a --user-agent="GFW-URL-checker (http://yeri.be/hr); country: $COUNTRY" --no-check-certificate -O /dev/null 2>&1 | grep "200 OK"` == "" ]] && echo $a,`date -u +%d-%m-%Y,%H:%M:%S`,nok >> $2_nok.csv || echo $a,`date -u +%d-%m-%Y,%H:%M:%S`,ok >> $2_ok.csv
 } done < $1
