565d1acd37f165975ae2015ad1eaff2e75925e74
Yeri Tiete <yeri@tiete.be>
2011-02-13 02:46:32 +0100

    make sure host list is unique

--- sortHosts.sh
+++ sortHosts.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sort -f hosts/hostlist.txt > hosts/_hostlist.txt
 mv hosts/_hostlist.txt hosts/hostlist.txt
+awk '!x[$0]++' hosts/hostlist.txt > hosts/_hostlist.txt
+mv hosts/_hostlist.txt hosts/hostlist.txt
