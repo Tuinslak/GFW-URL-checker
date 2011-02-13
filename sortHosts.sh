6490821c4d9e998969da4878b5401efba472ecba
Yeri Tiete <yeri@tiete.be>
2011-02-13 02:58:57 +0100

    hosts + tolower()

--- sortHosts.sh
+++ sortHosts.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+# Yeri Tiete
+
+# sort alphabetically
 sort -f hosts/hostlist.txt > hosts/_hostlist.txt
 mv hosts/_hostlist.txt hosts/hostlist.txt
+
+# to lower()
+awk '{print tolower($0)}' hosts/hostlist.txt > hosts/_hostlist.txt
+mv hosts/_hostlist.txt hosts/hostlist.txt
+
+# make unique
 awk '!x[$0]++' hosts/hostlist.txt > hosts/_hostlist.txt
 mv hosts/_hostlist.txt hosts/hostlist.txt
